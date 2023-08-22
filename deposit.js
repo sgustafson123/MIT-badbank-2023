541ccdc6c1793201aea50f00ba4c7f3ee8ed70f3
Sonja Gustafson <124421321+sgustafson123@users.noreply.github.com>
2023-08-22 00:03:52 -0500

    deposit title

--- deposit.js
+++ deposit.js
@@ -36,7 +36,7 @@ const Account = () => {
   return (
     <div className="container mt-5">
       <div className="card">
-        <div className="card-header">Account</div>
+        <div className="card-header">Deposit</div>
         <div className="card-body">
           <form onSubmit={handleSubmit}>
             <h2>{status}</h2>
